878c04140a6c88cacde2c23abccaa518048a50d0
Alexander Wachenfeld <>
2022-10-31 15:10:41 -0400

    Basic outline for homepage navigation

--- src/home.py
+++ src/home.py
@@ -1,5 +1,9 @@
 import os
 import sys
+from telecom_main import telecom_main
+
+#from cars_main import cars_main
+#from fantasy_main import fantasy_main
 
 sys.path.insert(
     0,
@@ -8,8 +12,23 @@ sys.path.insert(
 
 
 def main():
-    choice = input("Enter username: ")
-    print("Username is: " + choice)
+    print(f"""Based on avaiable dataset please choose from the available list:
+    1: Telecom Users
+    2:
+    3: Car Sales
+    4: Fantasy Football""")
+    choice = input("")
+    if (choice == "1"):
+        telecom_main()
+    elif (choice == "2"):
+
+    elif (choice == "3"):
+        #cars_main()
+    elif (choice == "4"):
+        #fantasy_main()
+    else:
+        print("You choice was not valid. Exiting Process.")
+        sys.exit()
 
 if __name__ == '__main__':
     main()
