93e7283dbe5e087a50c1c3298519d37b5b01baf2
Alexander Wachenfeld <>
2022-11-14 14:14:31 -0500

    Added ability for user to exit program as well as give them the option to choose a new dataset after compeltion.

--- src/home.py
+++ src/home.py
@@ -16,8 +16,9 @@ def main():
     1: Telecom Users
     2:
     3: Car Sales
-    4: Fantasy Football""")
-    choice = input("")
+    4: Fantasy Football
+    5: Exit Program""")
+    choice = input("Choose a number:")
     if (choice == "1"):
         telecom_main()
     #elif (choice == "2"):
@@ -26,8 +27,18 @@ def main():
         #cars_main()
     #elif (choice == "4"):
         #fantasy_main()
+    elif (choice == "5"):
+        print("Thank You, Goodbye!")
+        sys.exit()
     else:
         print("You choice was not valid. Exiting Process.")
+        sys.exit()
+    print("Would you like to choose a new dataset?")
+    choice_continue = input("(Y/N): ")
+    if (choice_continue.upper() == "Y"):
+        main()
+    else:
+        print("Thank You, Goodbye!")
         sys.exit()
 
 if __name__ == '__main__':
