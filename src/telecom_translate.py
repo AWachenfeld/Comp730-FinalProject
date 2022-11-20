d2fd673915114df127feb61a337278be3e36767f
Alexander Wachenfeld <>
2022-11-20 10:01:42 -0500

    Adde dimplementation for avg tenure to telecom users.

--- src/telecom_translate.py
+++ src/telecom_translate.py
@@ -117,3 +117,38 @@ class Monthlycharges():
         monthly_charges_d["Senior_charges"] = round(monthly_charges_d["Senior_charges"] / monthly_charges_d["Senior_total"], 2)
         monthly_charges_d["NonSenior_charges"] = round(monthly_charges_d["NonSenior_charges"] / monthly_charges_d["NonSenior_total"], 2)
         return monthly_charges_d
+
+
+class Tenure():
+    def __init__(self, filename):
+        self.devs = []
+        try:
+            with open(filename, 'r') as fin:
+                first_line = fin.readline()[:-1]
+                keys_lst = first_line.split('|')
+        except IOError as err:
+            print(err)
+        try:
+            with open(filename, 'r') as fin:
+                for line in fin.readlines()[1:]:
+                    values_lst = line[:-1].split('|')
+                    dev_d = dict(zip(keys_lst, values_lst))
+                    self.devs.append(dev_d)
+        except IOError as err:
+            print(err)
+
+    def tenure_avg(self):
+        tenure_d = {"Senior_months": 0,"NonSenior_months": 0,
+                    "Senior_total": 0, "NonSenior_total": 0}
+        for user in self.devs:
+            senior_status = user.get("SeniorCitizen")
+            months = user.get("tenure")
+            if senior_status == "1":
+                    tenure_d["Senior_months"] += int(months)
+                    tenure_d["Senior_total"] += 1
+            else:
+                    tenure_d["NonSenior_months"] += int(months)
+                    tenure_d["NonSenior_total"] += 1
+        tenure_d["Senior_months"] = round(tenure_d["Senior_months"] / tenure_d["Senior_total"], 2)
+        tenure_d["NonSenior_months"] = round(tenure_d["NonSenior_months"] / tenure_d["NonSenior_total"], 2)
+        return tenure_d
