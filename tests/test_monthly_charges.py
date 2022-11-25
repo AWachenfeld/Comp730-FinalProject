5490b570caa1a52eda9e5534bbca90581a2135ea
Alexander Wachenfeld <>
2022-11-25 13:37:15 -0500

    Fixed issue with contract_renewal test, added unittest for monthly_charges implementation.

--- tests/test_monthly_charges.py
+++ tests/test_monthly_charges.py
@@ -1,17 +1,16 @@
 import unittest
 import os
 import sys
-from src.telecom_translate import Contractrenewal
+from src.telecom_translate import Monthlycharges
 
 sys.path.insert(
     0,
     os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 )
 
-
-class TestContractRenewal(unittest.TestCase):
+class TestMonthlyCharges(unittest.TestCase):
     """
-    tests contract_reneval() method
+    tests monthly_charges() method
     """
 
     def test_10_entry(self):
@@ -19,11 +18,10 @@ class TestContractRenewal(unittest.TestCase):
         Test case 1 using telecom_10user.txt with 10 row of data
         """
         input1 = './data/telecom_10users.txt'
-        devs_obj = Contractrenewal(input1)
-        actual_result = devs_obj.contract_renewal()
-        expected_result = {'NonSenior_cancel': 7, 'NonSenior_renew': 2,
-                           'Senior_cancel': 1, 'Senior_renew': 0}
-
+        devs_obj = Monthlycharges(input1)
+        actual_result = devs_obj.monthly_charges()
+        expected_result = {"Senior_charges": 84.95,"NonSenior_charges": 67.73,
+                            "Senior_total": 1, "NonSenior_total": 9}
         self.assertDictEqual(actual_result, expected_result)
 
     def test_50_entry(self):
@@ -31,10 +29,10 @@ class TestContractRenewal(unittest.TestCase):
         Test case 2 using telecom_50user.txt with 50 row of data
         """
         input1 = './data/telecom_50users.txt'
-        devs_obj = Contractrenewal(input1)
-        actual_result = devs_obj.contract_renewal()
-        expected_result = {'NonSenior_cancel': 28, 'NonSenior_renew': 15,
-                           'Senior_cancel': 4, 'Senior_renew': 3}
+        devs_obj = Monthlycharges(input1)
+        actual_result = devs_obj.monthly_charges()
+        expected_result = {"Senior_charges": 87.54,"NonSenior_charges": 65.45,
+                            "Senior_total": 7, "NonSenior_total": 43}
         self.assertDictEqual(actual_result, expected_result)
 
 if __name__ == '__main__':
