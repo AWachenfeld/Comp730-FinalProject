8d9fe5f17a65fbfcbf07d0072c7a4af12e522c66
Alexander Wachenfeld <>
2022-12-04 16:37:33 -0500

    Created 2nd implementation for gpu benchmarks being highest to lowest priced gpu, with ability to display top 5, 10, 25, and 50 GPUs.

--- src/gpu_home.py
+++ src/gpu_home.py
@@ -10,7 +10,8 @@ sys.path.insert(
 def gpu_main():
     print(f"""\nChoose what Telecom user data you would like to see:
     1: Top GPU scores in G3Dmark
-    2: Return to home""")
+    2: Highest priced GPUs
+    3: Return to home""")
     gpu_choice = input("Enter num choice: ")
 
     if (gpu_choice == "1"):
@@ -31,6 +32,29 @@ def gpu_main():
         else:
             print("You choice was not valid. Returning to Telecom options menu.\n")
             gpu_main()
+    
+    elif (gpu_choice == "2"):
+        print(f"""How many would you like to see:
+        1: 5 GPUs
+        2: 10 GPUs
+        3: 20 GPUs
+        4: 50 GPUs""")
+        benchmark_choice = input("Enter choice: ")
+        if (benchmark_choice == "1"):
+            Benchmarks.gpu_price(benchmark_choice)
+            gpu_main()
+        elif (benchmark_choice == "2"):
+            Benchmarks.gpu_price(benchmark_choice)
+            gpu_main()
+        elif (benchmark_choice == "3"):
+            Benchmarks.gpu_price(benchmark_choice)
+            gpu_main()
+        elif (benchmark_choice == "4"):
+            Benchmarks.gpu_price(benchmark_choice)
+            gpu_main()
+        else:
+            print("You choice was not valid. Returning to Telecom options menu.\n")
+            gpu_main()
 
-    elif (telecom_choice == "2"):
+    elif (telecom_choice == "3"):
         print("Thank You for using GPU Information\n")
