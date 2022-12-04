6c3baea34cfec60fffe97ec501ba535eb4a56485
Alexander Wachenfeld <>
2022-12-04 16:39:17 -0500

    added final implementation for gpu benchmarks being price to performance which is based on the G3D score/price in the csv file.

--- src/gpu_home.py
+++ src/gpu_home.py
@@ -11,7 +11,8 @@ def gpu_main():
     print(f"""\nChoose what Telecom user data you would like to see:
     1: Top GPU scores in G3Dmark
     2: Highest priced GPUs
-    3: Return to home""")
+    3: Best price to performance GPUs
+    4: Return to home""")
     gpu_choice = input("Enter num choice: ")
 
     if (gpu_choice == "1"):
@@ -56,5 +57,28 @@ def gpu_main():
             print("You choice was not valid. Returning to Telecom options menu.\n")
             gpu_main()
 
-    elif (telecom_choice == "3"):
+    elif (gpu_choice == "3"):
+        print(f"""How many would you like to see:
+        1: 5 GPUs
+        2: 10 GPUs
+        3: 20 GPUs
+        4: 50 GPUs""")
+        benchmark_choice = input("Enter choice: ")
+        if (benchmark_choice == "1"):
+            Benchmarks.gpu_performance_price(benchmark_choice)
+            gpu_main()
+        elif (benchmark_choice == "2"):
+            Benchmarks.gpu_performance_price(benchmark_choice)
+            gpu_main()
+        elif (benchmark_choice == "3"):
+            Benchmarks.gpu_performance_price(benchmark_choice)
+            gpu_main()
+        elif (benchmark_choice == "4"):
+            Benchmarks.gpu_performance_price(benchmark_choice)
+            gpu_main()
+        else:
+            print("You choice was not valid. Returning to Telecom options menu.\n")
+            gpu_main()
+
+    elif (telecom_choice == "4"):
         print("Thank You for using GPU Information\n")
--- src/gpu_translate.py
+++ src/gpu_translate.py
@@ -41,3 +41,21 @@ class Benchmarks():
             print(sorted_df.head(25))
         elif (choice == "4"):
             print(sorted_df.head(50))
+
+    def gpu_performance_price(choice):
+        pd.set_option('display.expand_frame_repr', False)
+        df = pd.read_csv('../data/gpu_benchmarks.csv')
+        df = pd.DataFrame.set_index(df, keys="gpuName")
+        df = df.drop('TDP', axis=1)
+        df = df.drop('powerPerformance', axis=1)
+        df = df.drop('testDate', axis=1)
+        df = df.drop('category', axis=1)
+        value_df = df.sort_values(by=["gpuValue"], ascending=False)
+        if (choice == "1"):
+            print(value_df.head(5))
+        elif (choice == "2"):
+            print(value_df.head(10))
+        elif (choice == "3"):
+            print(value_df.head(25))
+        elif (choice == "4"):
+            print(value_df.head(50))
